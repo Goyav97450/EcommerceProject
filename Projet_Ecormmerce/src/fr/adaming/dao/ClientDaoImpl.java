6bbc77a8a7dc1689749d28b41f592ef55c1bc662
Adaming <Adaming@192.168.1.168>
2018-08-09 13:07:00 +0200

    Thibault : commit correction persistence

---
+++ Projet_Ecormmerce/src/fr/adaming/dao/ClientDaoImpl.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package fr.adaming.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import fr.adaming.model.Categorie;
+import fr.adaming.model.Client;
+import fr.adaming.model.Commande;
+import fr.adaming.model.Produit;
+
+/**
+ * @author Thibault
+ * Classe implémentant l'interface IClientDao
+ * L'anotation @Stateless permet au conteneur EJB de comprendre que c'est un EJB session de type <i>Stateless</i>
+ */
+@Stateless
+public class ClientDaoImpl implements IClientDao{
+
+	/** Récupération d'un entityManager à partir de la PersistenteUnit
+	 *  grâce à l'annotation @PersistenceContext
+	 */
+	@PersistenceContext(unitName="Projet_Ecormmerce")
+	private EntityManager em;
+	
+	public List<Categorie> getAllCategorie() {
+		//Création d'une requête JPQL
+		String req = "SELECT cat FROM Categorie cat";
+		
+		//Récupération d'une 
+		
+		return null;
+	}
+
+	public List<Produit> getProdByCategorie(Categorie cat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Produit> getProdByKeyWord(String keyWord) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int saveClient(Client cl) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int saveCommande(Commande co) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+}
