13ec928679f1dc4df95fd4efe702b63ec353b1d3
Adaming <Adaming@192.168.1.168>
2018-08-09 11:48:25 +0200

    thibault : rajout de la javaDoc pour les classes Client et Commande

--- Projet_Ecormmerce/src/fr/adaming/model/Commande.java
+++ Projet_Ecormmerce/src/fr/adaming/model/Commande.java
@@ -21,64 +21,106 @@ import javax.persistence.TemporalType;
 public class Commande implements Serializable{
 
 	//Attributs
+	/**
+	 * L'id de l'objet Commande, c'est aussi la clé primaire dans la DB
+	 */
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id_co")
 	private Long idCommande;
+	/**
+	 * La date de création de l'objet Commande
+	 */
 	@Temporal(TemporalType.DATE)
 	private Date dateCommande;
 	
-	//Transormation de l'association UML en JAVA
+	/**
+	 * Transormation de l'association UML en JAVA entre un objet Commade et un objet Client
+	 */
 	@ManyToOne
 	@JoinColumn(name="cl_id", referencedColumnName="id_cl")
 	private Client client;
-	
+	/**
+	 * Transormation de l'association UML en JAVA entre un objet Commade et une liste de ligneCommande
+	 */
 	@OneToMany(mappedBy="commande")
 	private List<LigneCommande> listeLigne;
 	
 	//Constructeurs
+
+	/**
+	 * Constructeur Vide
+	 */
 	public Commande() {
 		super();
 	}
+	
+	/**
+	 * Constructeur sans id utilisé pour la méthode ajout d'une Commande dans la DB
+	 * @param dateCommande
+	 */
 	public Commande(Date dateCommande) {
 		super();
 		this.dateCommande = dateCommande;
 	}
+
+	/**
+	 * Cosntructeur avec id utilisé pour les méthodes modifier et récupérer une Commande dans la DB
+	 * @param idCommande
+	 * @param dateCommande
+	 */
 	public Commande(Long idCommande, Date dateCommande) {
 		super();
 		this.idCommande = idCommande;
 		this.dateCommande = dateCommande;
 	}
-	
+
 	//Getter/Setter
+	/**
+	 * @return the id of the current Commande
+	 */
 	public Long getIdCommande() {
 		return idCommande;
 	}
-	
+	/**
+	 * @param the id of the current Commande
+	 */
 	public void setIdCommande(Long idCommande) {
 		this.idCommande = idCommande;
 	}
-	
+	/**
+	 * @return the date of the current Commande
+	 */
 	public Date getDateCommande() {
 		return dateCommande;
 	}
-	
+	/**
+	 * @param set the date of the current commande
+	 */
 	public void setDateCommande(Date dateCommande) {
 		this.dateCommande = dateCommande;
 	}
-	
+	/**
+	 * @return the Client associated with the current Commande
+	 */
 	public Client getClient() {
 		return client;
 	}
-	
+	/**
+	 * @param set the Client associated with the current Commande
+	 */
 	public void setClient(Client client) {
 		this.client = client;
 	}
-	
+	/**
+	 * @return the list of LigneCommande associated with the current Commande
+	 */
 	public List<LigneCommande> getListeLigne() {
 		return listeLigne;
 	}
-	
+	/**
+	 * @param set the list of LignCommande associated with the current Commande
+	 */
 	public void setListeLigne(List<LigneCommande> listeLigne) {
 		this.listeLigne = listeLigne;
 	}
@@ -87,5 +129,5 @@ public class Commande implements Serializable{
 	@Override
 	public String toString() {
 		return "Commande [idCommande=" + idCommande + ", dateCommande=" + dateCommande + "]";
-	}	
+	}
 }
