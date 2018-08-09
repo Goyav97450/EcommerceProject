bd90181f6fa68ec76de4825068c17f134e4a7c9c
Adaming <Adaming@192.168.1.153>
2018-08-09 12:35:12 +0200

    ewen: ajout de l'interface Dao Admin

---
+++ Projet_Ecormmerce/src/fr/adaming/dao/IAdminDao.java
@@ -0,0 +1,23 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.model.Admin;
+import fr.adaming.model.Categorie;
+import fr.adaming.model.Produit;
+
+/**
+ * @author Ewen Fondrillon
+ * Interface Dao des méthodes reliées aux fonctionnalités Admin
+ */
+public interface IAdminDao {
+
+	/**<b>isExist</b>
+	 * Cette méthode permet de vérifier qu'un Admin existe bien dans la base de données
+	 * via son ID et mot de passe, pour lui autoriser l'accès aux fonctionnalités admin.
+	 * @param l'Admin dont on cherche à confirmer l'existence
+	 * @return l'Admin dont l'existence est validée.
+	 */
+	public Admin isExist(Admin a);
+
+}
